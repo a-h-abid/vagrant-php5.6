dafcd42e7146376887b25bc4c2335a799e19ec47
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2016-01-13 10:33:24 +0600

    Update box to trusty and  install.sh

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,9 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "precise64"
+    config.vm.box = "ubuntu/trusty64"
 
-    config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+    #config.vm.box_url = "http://files.vagrantup.com/precise64.box"
     
     config.vm.network :private_network, ip: "192.168.10.54"
     config.vm.network :forwarded_port, guest: 80, host: 8056
--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties
 
 echo "--- Package For PHP 5.6 ---"
-sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo add-apt-repository -y ppa:ondrej/php5-5.6
 
 echo "--- Updating packages list ---"
 sudo apt-get update
@@ -37,7 +37,9 @@ echo "--- What developer codes without errors turned on? Not you, master. ---"
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
+echo "-- Configure Apache"
 sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+sudo sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www/' /etc/apache2/sites-enabled/000-default.conf
 
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
